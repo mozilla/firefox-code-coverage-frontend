fe6a0eae1d3ac3c7e4e8cfae709c22549334de13
Armen Zambrano G <armenzg@gmail.com>
2017-07-26 13:29:28 -0400

    Adding style-gitweb.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { CodeCoverageDiffViewer } from './diff.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Using hg-web's CSS file
+import './style-gitweb.css';
 
 // https://hg.mozilla.org/mozilla-central/rev/12e33b9d6f91
 var changeset = '12e33b9d6f91'
