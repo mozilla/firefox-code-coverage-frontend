67b7f7c6bae11aa79cd6c5d80b36af34ee178011
Armen Zambrano G <armenzg@gmail.com>
2017-08-15 14:54:06 -0400

    Show missing coverage and improved style & line numbers

--- src/DiffLine.js
+++ src/DiffLine.js
@@ -6,44 +6,50 @@ export function DiffLine(props) {
   var c = props.change
   var cov = props.code_cov_info
   // Added, deleted or unchanged line
-  var type = props.change.type
+  var change_type = props.change.type
   // CSS tr and td classes
-  var row_class, coverage_status
-  var line_id
+  var [row_class, cov_status_class] = ['nolinechange', 'nocovchange']
+  var row_id = props.id
+  // Cell contents
+  var cov_status = ' ' // We need blank string to respect width value of cell
   var [old_line_number, new_line_number] = ['', '']
-  //var coverage_status = 'unchanged'
 
-  if (type === 'add') {
+  if (change_type === 'add') {
     // Added line - <cov_status> | <blank> | <new line number>
-    row_class = type
+    row_class = change_type
+    cov_status_class = 'miss' // Let's start assuming a miss
     if (cov) {
       cov.changes.new.map(line_cov_info => {
-        // XXX: Verify in here that the lines match
+
         if (line_cov_info.line === c.ln) {
+          // c.content has a '+' sign at the beginning of the line
+          if (line_cov_info.content !== c.content.substring(1, c.content.length)) {
+            console.error('The diff line and code coverage line differ.')
+            console.log(line_cov_info.content)
+            console.log(c.content.substring(1,c.content.length))
+          }
           if (line_cov_info.coverage) {
-            coverage_status = 'hit'
-          } else {
-            // XXX: This is currently not doing anything
-            coverage_status = 'miss'
+            cov_status_class = 'hit'
           }
         }
       })
     }
-    line_id = props.id
     new_line_number = c.ln
-  } else if (type === 'del') {
-    // Removed line - <old line number> | <blank>
-    row_class = type
+  } else if (change_type === 'del') {
+    // Removed line - <blank> | <old line number> | <blank>
+    row_class = change_type
     old_line_number = c.ln
   } else {
-    // Unchanged line - <old line number> | <blank>
-    row_class = type
-    new_line_number = c.ln1
-    old_line_number = c.ln2
+    // Unchanged line - <blank> | <old line number> | <blank>
+    row_class = change_type
+    old_line_number = c.ln1
+    if (old_line_number !== c.ln2) {
+      new_line_number = c.ln2
+    }
   }
   return (
-    <tr id={line_id} className={row_class}>
-      <td className={coverage_status}>&nbsp;</td>
+    <tr id={row_id} className={row_class}>
+      <td className={cov_status_class}>{cov_status}</td>
       <td className='old_line_number'>{old_line_number}</td>
       <td className='new_line_number'>{new_line_number}</td>
       <td className='line_content'>
