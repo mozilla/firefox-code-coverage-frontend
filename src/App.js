d3609a4a993e34c2ebbfe740c0d9e819229f3434
Armen Zambrano G <armenzg@gmail.com>
2017-08-16 14:37:56 -0400

    Form to submit changeset and routing support

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import { DiffViewer } from './DiffViewer'
-import { Home } from './Home'
-
-// https://hg.mozilla.org/mozilla-central/rev/12e33b9d6f91
-var changeset = '12e33b9d6f91'
+import { ChangesetForm } from './ChangesetForm'
 
 // Main component
-export function App(props) {
-  return (
-    <div className='app'>
-      <Route exactly path="/" component={Home} />
-      <Route path="/changeset" render={() => (
-        <DiffViewer changeset={changeset}/>
-      )}/>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    // https://hg.mozilla.org/mozilla-central/rev/12e33b9d6f91
+    changeset: '12e33b9d6f91' // Until we have coverage for other changesets
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <Route exact path="/" render={({ history }) => (
+          <ChangesetForm
+            initialValue={this.state.changeset}
+            onSubmit={(form_values) => {
+              this.setState({changeset: form_values.changeset})
+              if (form_values.changeset) {
+                history.push('/changeset/' + form_values.changeset)
+              } else {
+                console.log('XXX: The changeset was undefined.')
+              }
+            }}
+          />
+        )}/>
+        <Route path="/changeset" render={() => (
+          <DiffViewer changeset={this.state.changeset}/>
+        )}/>
+      </div>
+    );
+}
 }
