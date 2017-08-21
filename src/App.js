90849241e7a5c53a0b4933b25e083f5a9128e884
Armen Zambrano G <armenzg@gmail.com>
2017-08-21 16:45:33 -0400

    Display most recent pushes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
-import { DiffViewer } from './DiffViewer'
+
 import { ChangesetForm } from './ChangesetForm'
+import { ChangesetsViewer } from './ChangesetsViewer'
+import { DiffViewer } from './DiffViewer'
 
 // Main component
 export class App extends Component {
   state = {
-    // https://hg.mozilla.org/mozilla-central/rev/12e33b9d6f91
-    changeset: '12e33b9d6f91' // Until we have coverage for other changesets
+    repo_name: 'mozilla-central',
   }
 
   render() {
     return (
       <div className='app'>
         <Route exact path="/" render={({ history }) => (
-          <ChangesetForm
-            initialValue={this.state.changeset}
-            onSubmit={(form_values) => {
-              this.setState({changeset: form_values.changeset})
-              if (form_values.changeset) {
-                history.push('/changeset/' + form_values.changeset)
-              } else {
-                console.log('XXX: The changeset was undefined.')
-              }
-            }}
-          />
+          <div className='changesets-viewer'>
+            <ChangesetForm
+              onSubmit={(form_values) => {
+                this.setState({changeset: form_values.changeset})
+                if (form_values.changeset) {
+                  history.push('/changeset/' + form_values.changeset)
+                } else {
+                  console.log('XXX: The changeset was undefined.')
+                }
+              }}
+            />
+            <hr/>
+            <ChangesetsViewer
+              repo_name={this.state.repo_name}
+            />
+          </div>
         )}/>
         <Route path="/changeset" render={() => (
           <DiffViewer changeset={this.state.changeset}/>
--- src/fetch_data.js
+++ src/fetch_data.js
@@ -5,3 +5,8 @@ const headers = {
 
 export const getDiff = (changeset) =>
   fetch(`${host}/mozilla-central/raw-rev/${changeset}`, { headers })
+
+export const getJsonPushes = (repo_name) =>
+  fetch(`${host}/${repo_name}/json-pushes?version=2&full=1`, {
+    'Accept': 'application/json'
+  })
