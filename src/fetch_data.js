043555fef678ae09cea0a8f2a45ec276e66cba0a
Armen Zambrano G <armenzg@gmail.com>
2017-08-28 10:58:14 -0400

    feat: Show uploaded commits to codecov

--- src/fetch_data.js
+++ src/fetch_data.js
@@ -1,12 +1,14 @@
-const host = 'https://hg.mozilla.org'
-const headers = {
+const hg_host = 'https://hg.mozilla.org'
+
+const text_plain_headers = {
   'Accept': 'text/plain'
+}
+const json_headers = {
+  'Accept': 'application/json'
 }
 
 export const getDiff = (changeset) =>
-  fetch(`${host}/mozilla-central/raw-rev/${changeset}`, { headers })
+  fetch(`${hg_host}/mozilla-central/raw-rev/${changeset}`, text_plain_headers)
 
 export const getJsonPushes = (repo_name) =>
-  fetch(`${host}/${repo_name}/json-pushes?version=2&full=1`, {
-    'Accept': 'application/json'
-  })
+  fetch(`${hg_host}/${repo_name}/json-pushes?version=2&full=1`, json_headers)
