64fa2202024331a29f832d51939c097df7404dcd
Armen Zambrano G <armenzg@gmail.com>
2017-09-05 17:16:21 -0400

    feat: Show all changesets within a push

--- src/components/CoverageSummary/ChangesetsViewer.js
+++ src/components/CoverageSummary/ChangesetsViewer.js
@@ -2,25 +2,27 @@ import React, { Component } from 'react';
 
 import * as FetchAPI from '../../fetch_data'
 
-function PushInfo(props) {
-  const csets = props.push_info.changesets
-  const { author, node, desc } = csets[csets.length - 1]
+function ChangesetInfo({ index, author, node, description, hidden }) {
   // XXX: For author remove the email address
   // XXX: For desc display only the first line
   // XXX: linkify bug numbers
   return (
-    <tr className='push'>
-      <td className='tip-changeset-author'>{author.substring(0, 16)}</td>
-      <td className='tip-changeset-node-id'>{node.substring(0, 12)}</td>
-      <td className='tip-changeset-description'>{desc.substring(0, 30)}</td>
+    <tr className='changeset'>
+      <td className='tip-changeset-author'>
+        {author.substring(0, 16)}</td>
+      <td className='tip-changeset-node-id'>
+        {node.substring(0, 12)}</td>
+      <td className='tip-changeset-description'>
+        {description.substring(0, 30)}</td>
     </tr>
   )
 }
 
 export class ChangesetsViewer extends Component {
   state = {
-    'pushes': [],
-    'error_message': ''
+    'pushes': {},
+    'error_message': '',
+    'hidden_changesets': {}
   }
 
   componentDidMount() {
@@ -28,8 +30,15 @@ export class ChangesetsViewer extends Component {
     //      no call setSate to prevent one more render
     FetchAPI.getJsonPushes(this.props.repo_name).then(response =>
       response.json()
-    ).then(text =>
-      this.setState({pushes: text.pushes})
+    ).then(text => {
+      let hidden_changesets = {}
+      for (var push_id in text.pushes) {
+        hidden_changesets[push_id] = false
+      }
+      this.setState({
+        pushes: text.pushes,
+        hidden_changesets: hidden_changesets
+      })}
     ).catch(error => {
       this.setState({
         pushes: [],
@@ -56,13 +65,21 @@ export class ChangesetsViewer extends Component {
               if (csets[csets.length - 1].author !== 'ffxbld') {
                 return this.state.pushes[push_id]
               }
-            }).map((push_id) => (
-              <PushInfo
-                key={push_id}
-                push_info={this.state.pushes[push_id]}
-                collapsed={this.state.collapsed}
-              />
-            ))}
+            }).map(pushId => {
+              const csets = this.state.pushes[pushId].changesets
+              return csets.map((cset, index) => {
+                const { author, node, desc } = csets[index]
+                return (
+                  <ChangesetInfo
+                    key={node}
+                    index={index}
+                    author={author}
+                    node={node}
+                    description={desc}
+                  />
+                )
+              })
+            })}
           </tbody>
         </table>
       )
