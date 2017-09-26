8ff26b5653706bda5404a2acc41ca4df9a7d3b86
Armen Zambrano G <armenzg@gmail.com>
2017-09-26 16:58:57 -0400

    feat: Support fetching coverage summary for each push on the main view
    NOTE: This change can only work if the backend behaves (currently failing often),
    otherwise, you will not think anything useful happens in this commit besides adding
    an empty 'code coverage' column.
    
    Recent pushes from mozilla-central may not have code coverage available for few hours.
    
    If succesful, this change adds a coverage summary for every push.
    
    We fetch the coverage data sequentially for each push (rather than in parallel)
    since we can overwhelm the backend. Read the details in
    [here](https://github.com/mozilla-releng/services/issues/632).

--- src/components/summaryviewer.js
+++ src/components/summaryviewer.js
@@ -5,13 +5,25 @@ import * as FetchAPI from '../fetch_data';
 import dateFromUnixTimestamp from '../utils/date';
 
 const ChangesetInfo = ({ changeset, push, onClick }) => {
+  const hideCset = (desc) => {
+    if (desc.includes('=merge') || (desc.includes('erge') && desc.includes('to'))) {
+      return true;
+    }
+    return false;
+  };
   const { author, node, desc, index, showToggle, pushId } = changeset;
-  const { collapsed, linkify, date } = push;
+  const { collapsed, linkify, date, summary } = push;
   // XXX: For author remove the email address
   // XXX: For desc display only the first line
   // XXX: linkify bug numbers
   // The tipmost changeset should always be visible
-  const changesetClass = (index !== 0 && collapsed) ? 'collapsed_changeset' : 'changeset';
+  let changesetClass;
+  // XXX: For now do not hide timpost changesets
+  if (index !== 0 && hideCset(desc)) {
+    changesetClass = 'collapsed_changeset';
+  } else {
+    changesetClass = (index !== 0 && collapsed) ? 'collapsed_changeset' : 'changeset';
+  }
   const toggleText = (collapsed) ? '[Expand]' : '[Collapse]';
   return (
     <tr className={changesetClass}>
@@ -20,7 +32,7 @@ const ChangesetInfo = ({ changeset, push, onClick }) => {
       <td className="changeset-author">
         {author.substring(0, 22)}</td>
       <td className="changeset-node-id">
-        {(linkify) ?
+        {(linkify && index !== 0) ?
           <Link to={`/changeset/${node}`}>{node.substring(0, 12)}</Link>
           : <span>{node.substring(0, 12)}</span>}
       </td>
@@ -31,6 +43,14 @@ const ChangesetInfo = ({ changeset, push, onClick }) => {
           <span>{push.numCsets} changesets in push -&nbsp;
             <a href={`#${pushId}`} id={pushId} onClick={onClick}>{toggleText}</a>
           </span>
+        }
+      </td>
+      <td className="push-coverage-summary">
+        {(summary && index === 0 && summary.error) &&
+          <span>{summary.error}</span>
+        }
+        {(summary && index === 0 && summary.overall_cur) &&
+          <span>{summary.overall_cur}</span>
         }
       </td>
     </tr>
@@ -46,6 +66,7 @@ const ChangesetsViewer = ({ changesets, pushes, onClick }) => (
         <th>Changeset</th>
         <th>Description</th>
         <th>Collapsed csets</th>
+        <th>Coverage summary</th>
       </tr>
       {changesets.map(cset => (
         <ChangesetInfo
@@ -64,9 +85,7 @@ const processJsonPushes = (pushes) => {
     if (
       (author.includes('ffxbld')) ||
       (desc.includes('ack out')) ||
-      (desc.includes('acked out')) ||
-      (desc.includes('=merge')) ||
-      ((desc.includes('erge')) && (desc.includes('to')))) {
+      (desc.includes('acked out'))) {
       return true;
     }
 
@@ -86,13 +105,13 @@ const processJsonPushes = (pushes) => {
     const lenCsets = csets.length - 1;
     const tipmost = csets[lenCsets];
 
-    if (!ignore(tipmost)) {
+    if (lenCsets > 1 || !ignore(tipmost)) {
       filteredPushes[id] = {
         date: push.date,
         numCsets: lenCsets,
         tipmost: tipmost.node,
-        collapsed: false,
-        linkify: true,
+        collapsed: true,
+        linkify: false,
       };
       csets.reverse()
         .filter(c => !ignore(c))
@@ -116,6 +135,66 @@ const processJsonPushes = (pushes) => {
   };
 };
 
+// XXX: For now, we can only make 1 request a time to the backend
+// or we will overwhelm it: https://github.com/mozilla-releng/services/issues/632
+// Recursively fetching one summary at a time
+const getSummaries = async (pushIds, pushes) => {
+  // We're expecting the list of pushIds to be sorted from highest to lowest
+  // since we grab the pushId from the end.
+  // This is because more recent pushes are likely not to have code coverage data.
+  const pushId = pushIds.pop();
+  if (!pushId) {
+    return {};
+  }
+
+  const push = pushes[pushId];
+  const processedPushes = {};
+  processedPushes[pushId] = {
+    ...push,
+  };
+
+  try {
+    console.log(`About to fetch ${pushId} (${push.tipmost})`);
+    // XXX: fetch does not support timeouts. I would like to add a 5 second
+    // timeout rather than wait Heroku's default 30 second timeout. Specially
+    // since we're doing sequential fetches.
+    // XXX: Wrap fetch() in a Promise; add a setTimeout and call reject() if
+    // it goes off, otherwise resolve with the result of the fetch()
+    const res = await FetchAPI.getChangesetCoverage(push.tipmost);
+    if (res.status === 202) {
+      processedPushes[pushId].summary = {
+        error: 'Pending',
+      };
+    } else if (res.status === 200) {
+      const ccSum = await res.json();
+      processedPushes[pushId].summary = ccSum;
+
+      if (ccSum.overall_cur) {
+        // We have coverage data, thus, adding links to the coverage diff viewer
+        processedPushes[pushId].linkify = true;
+        processedPushes[pushId].collapsed = false;
+      }
+    } else {
+      processedPushes[pushId].summary = {
+        error: res.statusText,
+      };
+    }
+  } catch (e) {
+    console.log(e);
+    processedPushes[pushId].summary = {
+      error: 'Failed to fetch.',
+    };
+  }
+
+  try {
+    const remainingSummaries = await getSummaries(pushIds, pushes);
+    return Object.assign(processedPushes, remainingSummaries);
+  } catch (e) {
+    // Once one fails; let's just stop and return the original list
+    return pushes;
+  }
+};
+
 export default class ChangesetsViewerContainer extends Component {
   constructor(props) {
     super(props);
@@ -132,13 +211,24 @@ export default class ChangesetsViewerContainer extends Component {
     FetchAPI.getJsonPushes(repoName)
       .then(response =>
         response.json(),
-      ).then((text) => {
+      ).then(async (text) => {
         const { csets, pushList } = processJsonPushes(text.pushes);
 
         this.setState({
           changesets: csets,
           pushes: pushList,
         });
+
+        try {
+          // Ordered from lowest to higher
+          const pushIds = Object.keys(pushList).reverse();
+          const newPushes = await getSummaries(pushIds, pushList);
+          this.setState({
+            pushes: newPushes,
+          });
+        } catch (e) {
+          console.log(e);
+        }
       }).catch((error) => {
         console.log(error);
         this.setState({
