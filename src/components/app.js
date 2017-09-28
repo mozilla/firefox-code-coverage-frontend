d79284427f02c736f1b3b4e4e5fde9e9181e2c9e
Armen Zambrano G <armenzg@gmail.com>
2017-09-28 10:03:39 -0400

    feat: Add disclaimer

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,25 @@ type State = {
   repoName: string,
 };
 
+const AppDisclaimer = () => (
+  <div>
+    NOTE: This app is in an alpha state. There are some core issues we still
+    need to fix and until then we will list them in here.
+    <ul>
+      <li>The backend needs some refactoring for the endpoints not to timeout
+        often (<a href="https://github.com/mozilla-releng/services/issues/632">Issue 632</a>)
+      </li>
+      <li>Some changesets are not shown (Backouts, ffxbld & merges)</li>
+      <li>Links to some changesets are not guaranteed to have code coverage</li>
+    </ul>
+    <span>UI based on&nbsp;
+      <a href="https://github.com/armenzg/firefox-code-coverage-frontend">
+        Firefox code coverage frontend
+      </a></span>
+    <hr />
+  </div>
+);
+
 // Main component
 export default class App extends Component<Props, State> {
   state = {
@@ -25,6 +44,7 @@ export default class App extends Component<Props, State> {
         <Route
           exact path="/" render={() => (
             <div className="changesets-viewer">
+              <AppDisclaimer />
               <ChangesetsViewerContainer
                 repoName={repoName} />
             </div>
