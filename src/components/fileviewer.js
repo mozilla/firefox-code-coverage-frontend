e038252e0e94a465df5136649f4d19626b5fe016
301githublinkai <linkai@ualberta.ca>
2017-10-19 15:43:25 -0600

    Issue 1: The fileviewer now is able to show file contents on the page
    Given revision-number and the path, the fileviewer is able to fetch the sources from hg,
    and displays it on the fileview.

--- src/components/fileviewer.js
+++ src/components/fileviewer.js
@@ -1,15 +1,85 @@
 import React, { Component } from 'react';
 
+import * as FetchAPI from '../fetch_data';
+
 export default class FileViewerContainer extends Component {
-  // console.log(this.props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      appError: undefined,
+      rawFile: undefined,
+    };
+
+    const { revision, path } = this.props;
+    FetchAPI.getRawFile(revision, path)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log('Error status code' + response.status);
+          return;
+        }
+        response.text().then(file => {
+          this.setState({ rawFile: file.split('\n') })
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({
+          appError: 'We did not manage to parse the raw-file correctly.',
+        });
+      });
+  }
+
+
   render() {
-    console.log(this.props);
+    // console.log(this.props);
+    const { revision, path } = this.props;
     return (
       <div>
-        <div>File view</div>
-        <div>revision number: {this.props.revision}</div>
-        <div>path to file: {this.props.path}</div>
+        <FileViewerMeta revision={revision} path={path}/>
+        <FileViewer rawFile={this.state.rawFile} />
       </div>
     );
   }
 }
+
+
+
+/* This viewer renders each line of the file with its line number */
+const FileViewer = ({ rawFile }) => {
+  if (!rawFile) {
+    return( <div>"Waiting for file from the backend"</div> );
+  }
+
+  var id = 0;
+  const rawFileLines = rawFile.map((line) => {
+    id = id + 1;
+    return(
+      <tr key={id}>
+        <th>{id}</th>
+        <th>{line}</th>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table className="table table-sm table-bordered table-hover">
+        <tbody>
+          {rawFileLines}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+
+
+/* This view contains meta data of the file */
+const FileViewerMeta = ({ revision, path }) => {
+  return (
+    <h4>
+      Revision number: {revision} <br/> Path: {path}
+    </h4>
+  );
+};
