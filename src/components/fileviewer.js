4f0a6f3da7da93b470404ec286e21e74d1415e9d
301githublinkai <linkai@ualberta.ca>
2017-11-02 17:06:49 -0600

    Add fileviewer sidebar, selected line state, onClick handler function

--- src/components/fileviewer.js
+++ src/components/fileviewer.js
@@ -15,7 +15,11 @@ export default class FileViewerContainer extends Component {
       appError: undefined,
       coverage: undefined,
       parsedFile: [],
+      selectedLine: null,
+      testsPerLines: undefined,
     };
+
+    this.setSelectedLine = this.setSelectedLine.bind(this)
   }
 
   componentDidMount() {
@@ -49,10 +53,42 @@ export default class FileViewerContainer extends Component {
       "format": "list"
     })
     .then(data => {
-      this.setState({coverage: data});
       // TODO remove these log lines
       console.log(data);
+      this.setState({coverage: data});
+      this.parseTestsCoverage(data.data);
     });
+  }
+
+  /* Parse data returns from ActiveData */
+  parseTestsCoverage(data) {
+    console.log(data.length);
+    var stat = []
+
+    for (var i = 0; i < data.length; i++) {
+      var coveredLines = data[i].source.file.covered;
+
+      for (var j = 0; j < coveredLines.length; j++) {
+        var line = coveredLines[j];
+        if (!stat[line]) {
+          stat[line] = [];
+        }
+        stat[line].push(i);
+      }
+    }
+    console.log(stat);
+    this.setState({testsPerLines: stat});
+  }
+
+  /* handle fileviewer's line onclick event */
+  setSelectedLine(lineNumber) {
+    console.log(lineNumber);
+    this.setState({selectedLine: lineNumber});
+    // if (this.state.selectedLine == lineNumber) {
+    //   this.setState({selectedLine: null})
+    // } else {
+    //   this.setState({selectedLine: lineNumber});
+    // }
   }
 
   render() {
@@ -66,6 +102,13 @@ export default class FileViewerContainer extends Component {
         <FileViewer
           parsedFile={this.state.parsedFile}
           coverage={this.state.coverage}
+          testsPerLines={this.state.testsPerLines}
+          onLineClick={this.setSelectedLine}
+        />
+        <TestsViewer
+          coverage={this.state.coverage}
+          testsPerLines={this.state.testsPerLines}
+          selectedLine={this.state.selectedLine}
         />
       </div>
     );
@@ -73,7 +116,7 @@ export default class FileViewerContainer extends Component {
 }
 
 /* This component renders each line of the file with its line number */
-const FileViewer = ({ parsedFile, coverage }) => {
+const FileViewer = ({ parsedFile, coverage, testsPerLines, onLineClick }) => {
   return (
     <div>
       <table>
@@ -84,6 +127,7 @@ const FileViewer = ({ parsedFile, coverage }) => {
                 key={lineNumber}
                 lineNumber={lineNumber+1}
                 lineText={line}
+                onLineClick={onLineClick}
               />
             ))
           }
@@ -94,13 +138,125 @@ const FileViewer = ({ parsedFile, coverage }) => {
 };
 
 const Line = (props) => {
+  const handleOnClick = () => {
+    props.onLineClick(props.lineNumber);
+  }
+
   return (
       <tr>
         <td className="file_line_number">{props.lineNumber}</td>
-        <td><pre>{props.lineText}</pre></td>
+        <td className="file_line_text" onClick={handleOnClick}><pre>{props.lineText}</pre></td>
       </tr>
   );
 };
+
+
+
+
+
+
+/* Sidebar component, show which tests will cover the given selected line */
+const TestsViewer = ({coverage, testsPerLines, selectedLine}) => {
+
+  if (!testsPerLines) {
+    return (
+      <div className="tests_viewer">
+        <h3>"Fetching coverage from backend..."</h3>
+      </div>
+    );
+  }
+
+  // TODO if no line has been selected, show coverage of the file
+  else if (!selectedLine) {
+    return (
+      <div className="tests_viewer">
+        <h3>Select a line to view tests</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div className="tests_viewer">
+      <h3>Line: {selectedLine}</h3>
+      <TestsDetail
+        coverage={coverage}
+        testsPerLines={testsPerLines}
+        selectedLine={selectedLine}
+      />
+    </div>
+  );
+};
+
+const TestsDetail = ({coverage, testsPerLines, selectedLine}) => {
+  const testList = testsPerLines[selectedLine];
+    if (!testList) {
+      return <div>No test covers this line</div>
+    }
+
+  const testItems = testList.map((testNum) => {
+    const test = coverage.data[testNum];
+    const testName = test.run.name;
+    return <li key={testName}>{testName}</li>
+  });
+
+  return (
+    <ul>
+      {testItems}
+    </ul>
+  );
+}
+
+
+
+
+
+
+
+
+
+/* Sidebar component, show which tests will cover the given selected line */
+// const TestsViewer = ({coverage, testsPerLines, selectedLine}) => {
+//
+//   if (!testsPerLines || !selectedLine) {
+//     return (
+//       <ul className="tests_viewer">
+//         <li>waiting</li>
+//       </ul>
+//     );
+//   }
+//
+//   const testList = testsPerLines[selectedLine];
+//   if (!testList) {
+//     return <div className="tests_viewer">'No test for this line'</div>
+//   }
+//
+//   // console.log(coverage.data[3]);
+//   // return (<div />)
+//
+//   const testItems = testList.map((testNum) => {
+//     const test = coverage.data[testNum];
+//     // console.log(test);
+//     const testName = test.run.name;
+//     return <li key={testName}>{testName}</li>
+//   });
+//
+//
+//   return (
+//     <ul className="tests_viewer">
+//       {testItems}
+//     </ul>
+//   );
+// };
+
+
+
+
+
+
+
+
+
+
 
 /* This component contains metadata of the file */
 const FileViewerMeta = ({ revision, path, appError }) => {
