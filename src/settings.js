ea1a505f39086837fa449e68eec9b26e6755f8d2
Armen Zambrano G <armenzg@gmail.com>
2017-11-08 09:02:28 -0500

    refactor: Create settings module

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+const SETTINGS = {
+  coverageThresholds: {
+    low: {
+      threshold: 20,
+      className: 'low-coverage',
+    },
+    medium: {
+      threshold: 70,
+      className: 'medium-coverage',
+    },
+    high: {
+      className: 'high-coverage',
+    },
+  },
+};
+
+export default SETTINGS;
