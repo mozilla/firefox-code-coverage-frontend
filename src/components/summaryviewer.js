a9baaea8efcbd39e6f972fba452778deff390656
jhkren <kathy.ren@mail.utoronto.ca>
2018-02-14 00:16:17 -0500

    Issue #107: Changeset to link to Hg
    Changeset column links now redirect to Hg and Coverage Diff column created to access DiffViewer.

--- src/components/summaryviewer.js
+++ src/components/summaryviewer.js
@@ -8,14 +8,16 @@ import { arrayToMap, csetWithCcovData, mapToArray } from '../utils/data';
 
 const ChangesetInfo = ({ changeset }) => {
   const { author, desc, hidden, bzUrl, linkify, node, summary, summaryClassName } = changeset;
+  const hgUrl = changeset.coverage.hgRev;
   // XXX: For author remove the email address
   // XXX: For desc display only the first line
   return (
     <tr className={(hidden) ? 'hidden-changeset' : 'changeset'}>
       <td className="changeset-author">{author.substring(0, 22)}</td>
-      <td className="changeset-node-id">{(linkify) ?
-        <Link to={`/changeset/${node}`}>{node.substring(0, 12)}</Link>
-        : <span>{node.substring(0, 12)}</span>}
+      <td className="changeset-hg">
+        {(hgUrl) ?
+          <a href={hgUrl} target="_blank">{node.substring(0, 6)}</a> :
+          'No Hg coverage'}
       </td>
       <td className="changeset-description">
         {(bzUrl) ?
@@ -23,6 +25,10 @@ const ChangesetInfo = ({ changeset }) => {
           : desc.substring(0, 40) }
       </td>
       <td className={`changeset-summary ${summaryClassName}`}>{summary}</td>
+      <td className="changeset-node-id">{(linkify) ?
+        <Link to={`/changeset/${node}`}>{node.substring(0, 12)}</Link>
+        : <span>{node.substring(0, 12)}</span>}
+      </td>
     </tr>
   );
 };
@@ -35,7 +41,8 @@ const ChangesetsViewer = ({ changesets }) => (
           <th>Author</th>
           <th>Changeset</th>
           <th>Description</th>
-          <th>Coverage summary</th>
+          <th>Coverage Summary</th>
+          <th>Coverage Diff</th>
         </tr>
         {Object.keys(changesets).map(node => (
           <ChangesetInfo
