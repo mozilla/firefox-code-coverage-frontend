4d71c3d07176dc8c1a9dfda42e002c911317f34d
Dottori <jen.seo@mail.utoronto.ca>
2018-02-15 06:19:51 -0500

    Issue #79 - Move coverage percentage logic to coverage parse logic
    Issue #79 - Move coverage percentage logic to coverage parse logic
    
    Issue #79 - Move coverage percentage logic to coverage parse logic

--- src/utils/color.js
+++ src/utils/color.js
@@ -2,6 +2,6 @@ import * as Chroma from 'chroma-js';
 
 const getPercentCovColor = percent =>
   // warm red, lemon yellow, neon green
-  Chroma.scale(['#ff4f5e', '#fff44f', '#54ffbd']).mode('lab')(percent);
+  Chroma.scale(['#ff4f5e', '#fff44f', '#54ffbd']).mode('lab')(percent / 100);
 
 export default getPercentCovColor;
