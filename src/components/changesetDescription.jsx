736fa1ec5222a2d9d73621b922b2adf47f3f9f6e
Armen Zambrano G <armenzg@gmail.com>
2018-03-28 11:09:31 -0400

    Move Bugzilla icon to the left

--- src/components/changesetDescription.jsx
+++ src/components/changesetDescription.jsx
@@ -1,13 +1,14 @@
 import bzIcon from '../static/bugzilla.png';
+import dummyIcon from '../static/dummyIcon16x16.png';
 
 const ChangesetDescription = ({ description, bzUrl }) => (
   <div className="changeset-description">
-    {description.substring(0, 40).padEnd(40)}
     {(bzUrl) ?
       <a href={bzUrl} target="_blank">
         <img className="bzIcon" src={bzIcon} alt="bugzilla icon" />
       </a>
-      : undefined}
+      : <i className="icon-substitute" src={dummyIcon} />}
+    {description.substring(0, 40).padEnd(40)}
   </div>
 );
 
