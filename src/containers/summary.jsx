mozilla/firefox-code-coverage-frontend

commit 18cc0ed2dcd08a894acb7b6974e2fca6f085918b

Refactor: Separate coverage data into its own entity
This will simplify getting coverage data needed on different views.

For instance, we should not need to fetch changeset data when
we already have the changeset's node available and we can directly
fetch the coverage data.

In a future pass we will now be able to store in the local cache the coverage data
separate from the changesets data.

We also drop the notion of hidden changesets since we were not
taking advantage of it.

src/containers/summary.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import ReactInterval from 'react-interval';
 
-import SummaryViewer from '../components/summaryViewer';
+import Summary from '../components/summary';
 import settings from '../settings';
 import { arrayToMap, mapToArray } from '../utils/data';
-import { csetWithCcovData } from '../utils/coverage';
+import { getCoverage } from '../utils/coverage';
 import getChangesets from '../utils/hg';
 
-const { LOADING, INTERNAL_ERROR, PENDING } = settings.STRINGS;
+const { INTERNAL_ERROR, LOADING, PENDING } = settings.STRINGS;
 
 const PollingStatus = ({ pollingEnabled }) => (
   (pollingEnabled) ? (
@@ -17,11 +17,12 @@ const PollingStatus = ({ pollingEnabled }) => (
     </div>) : (null)
 );
 
-export default class ChangesetsViewerContainer extends Component {
+export default class SummaryContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      changesets: {},
+      changesets: [],
+      coverage: [],
       pollingEnabled: false, // We don't start polling until we're ready
       errorMessage: '',
       hideCsetsWithNoCoverage: true,
@@ -45,28 +46,29 @@ export default class ChangesetsViewerContainer extends Component {
       // 4) Inspect pushes from oldest to newest
       //    FAIL: If there are no pushes with some coverage (Show message)
       // 5) For every push with coverage data go and fetch each cset
-      const csetsNoCoverage = await getChangesets(repoName, hideCsetsWithNoCoverage);
-      const csets = await Promise.all(csetsNoCoverage.map(async cset =>
-        csetWithCcovData(cset)));
+      const changesets = await getChangesets(repoName, hideCsetsWithNoCoverage);
+      const coverage = await getCoverage(changesets);
       const summary = { pending: 0, error: 0 };
-      csets.forEach((cset) => {
+      changesets.forEach((cset) => {
         if (cset.summary === PENDING) {
           summary.pending += 1;
         } else if (cset.summary === INTERNAL_ERROR) {
           summary.error += 1;
         }
       });
-      console.log(`We have ${csets.length} changesets.`);
-      console.log(`pending: ${summary.pending}`);
-      console.log(`errors: ${summary.error}`);
+      console.debug(`We have ${changesets.length} changesets.`);
+      console.debug(`pending: ${summary.pending}`);
+      console.debug(`errors: ${summary.error}`);
       this.setState({
-        changesets: arrayToMap(csets),
+        changesets,
+        coverage,
         pollingEnabled: summary.pending > 0,
       });
     } catch (error) {
-      console.log(error);
+      console.error(error);
       this.setState({
-        changesets: {},
+        changesets: [],
+        coverage: [],
         pollingEnabled: false,
         errorMessage: 'We have failed to fetch coverage data.',
       });
@@ -74,23 +76,22 @@ export default class ChangesetsViewerContainer extends Component {
   }
 
   // We poll on an interval for coverage for csets without it
-  async pollPending(changesets) {
-    console.log('Determine if we need to poll csets w/o coverage data...');
+  async pollPending(coverage) {
+    console.debug('We are going to poll again for coverage data.');
     try {
-      let newCsets = mapToArray(changesets);
-      newCsets = await Promise.all(newCsets.map((c) => {
-        if (c.summary !== PENDING) {
-          return c;
-        }
-        return csetWithCcovData(c);
-      }));
-      const count = newCsets.filter(c => c.summary === PENDING).length;
-      const csetsMap = arrayToMap(newCsets);
+      // Only poll changesets that are still pending
+      const partialCoverage = await getCoverage(mapToArray(coverage)
+        .filter(cov => cov.summary === PENDING));
+      const count = partialCoverage.filter(cov => cov.summary === PENDING).length;
+      const newCoverage = {
+        ...coverage,
+        ...arrayToMap(partialCoverage),
+      };
       if (count === 0) {
-        console.log('No more polling required.');
-        this.setState({ changesets: csetsMap, pollingEnabled: false });
+        console.debug('No more polling required.');
+        this.setState({ coverage: newCoverage, pollingEnabled: false });
       } else {
-        this.setState({ changesets: csetsMap });
+        this.setState({ coverage: newCoverage });
       }
     } catch (e) {
       this.setState({ pollingEnabled: false });
@@ -99,13 +100,20 @@ export default class ChangesetsViewerContainer extends Component {
 
   render() {
     const {
-      changesets, pollingEnabled, errorMessage, timeout,
+      changesets, coverage, pollingEnabled, errorMessage, timeout,
     } = this.state;
+    const coverageMap = arrayToMap(coverage);
 
     if (errorMessage) {
       return (<div className="error-message">{errorMessage}</div>);
     }
-    const viewableCsets = mapToArray(changesets).filter(c => c.hidden === false);
+
+    const viewableCsetsMap = {};
+    changesets.forEach((cset) => {
+      if (coverageMap[cset.node].summary !== PENDING) {
+        viewableCsetsMap[cset.node] = cset;
+      }
+    });
 
     return (
       <div>
@@ -114,23 +122,26 @@ export default class ChangesetsViewerContainer extends Component {
             <ReactInterval
               timeout={timeout}
               enabled={pollingEnabled}
-              callback={() => this.pollPending(changesets)}
+              callback={() => this.pollPending(coverage)}
             />
             <PollingStatus
               pollingEnabled={pollingEnabled}
             />
           </div>
         )}
-        {viewableCsets.length > 0 &&
-          <SummaryViewer changesets={viewableCsets} />
+        {Object.keys(viewableCsetsMap).length > 0 &&
+          <Summary
+            changesets={viewableCsetsMap}
+            coverage={coverageMap}
+          />
         }
-        {(!pollingEnabled && Object.keys(changesets).length > 0) &&
+        {(!pollingEnabled && Object.keys(viewableCsetsMap) === 0) &&
           <p style={{ textAlign: 'center', fontWeight: 'bold' }}>
             <span>There is currently no coverage data to show. Please </span>
             <a href={`${settings.REPO}/issues/new`} target="_blank">file an issue</a>.
           </p>
         }
-        {(Object.keys(changesets).length === 0) &&
+        {Object.keys(changesets).length === 0 &&
           (<h3 className="loading">{LOADING}</h3>)
         }
       </div>

src/utils/coverage.js
@@ -2,18 +2,21 @@ import { uniq } from 'lodash';
 import settings from '../settings';
 import { JSON_HEADERS } from './fetch';
 
-const { ACTIVE_DATA, CCOV_BACKEND, HG_HOST } = settings;
+const {
+  ACTIVE_DATA, BACKEND, CCOV_BACKEND, CODECOV_GECKO_DEV, GH_GECKO_DEV,
+} = settings;
+
+export const githubUrl = gitCommit => `${GH_GECKO_DEV}/commit/${gitCommit}`;
+export const codecovUrl = gitCommit => (`${CODECOV_GECKO_DEV}/commit/${gitCommit}`);
+export const ccovBackendUrl = node => (`${BACKEND}/coverage/changeset/${node}`);
 
 const jsonPost = (url, body) =>
   fetch(url, { headers: JSON_HEADERS, method: 'POST', body: JSON.stringify(body) });
 
-export const getChangesetCoverage = changeset =>
-  fetch(`${CCOV_BACKEND}/coverage/changeset/${changeset}`, { JSON_HEADERS });
+const queryChangesetCoverage = node =>
+  fetch(`${CCOV_BACKEND}/coverage/changeset/${node}`, { JSON_HEADERS });
 
-export const getChangesetCoverageSummary = changeset =>
-  fetch(`${CCOV_BACKEND}/coverage/changeset_summary/${changeset}`, { JSON_HEADERS });
-
-export const queryActiveData = body =>
+const queryActiveData = body =>
   jsonPost(`${ACTIVE_DATA}/query`, body);
 
 const coverageSummary = (coverage) => {
@@ -130,57 +133,43 @@ export const transformCoverageData = (cov) => {
       percent: fileCoveragePercent(lines),
     };
   });
+  // Some extra data for the UI
+  const result = coverageSummaryText(newCov);
+  newCov.summary = result.text;
+  newCov.summaryClassName = result.className;
   return newCov;
 };
 
-export const csetWithCcovData = async (cset) => {
-  if (!cset.node) {
-    throw Error(`No node for cset: ${cset}`);
+export const getChangesetCoverage = async (node) => {
+  if (!node) {
+    throw Error(`No node for cset: ${node}`);
   }
-  const newCset = Object.assign({}, cset);
-  // XXX: fetch does not support timeouts. I would like to add a 5 second
-  // timeout rather than wait Heroku's default 30 second timeout. Specially
-  // since we're doing sequential fetches.
-  // XXX: Wrap fetch() in a Promise; add a setTimeout and call reject() if
-  // it goes off, otherwise resolve with the result of the fetch()
+  let coverage;
   try {
-    const res = await getChangesetCoverage(cset.node);
+    const res = await queryChangesetCoverage(node);
     if (res.status === 202) {
       // This is the only case when we poll again
-      newCset.summary = settings.STRINGS.PENDING;
+      coverage = { summary: settings.STRINGS.PENDING };
     } else if (res.status === 200) {
-      const coverageData = transformCoverageData(await res.json());
-
-      // XXX: Document in which cases we would not have overall_cur
-      if (coverageData.overall_cur) {
-        // We have coverage data, thus, adding links to the coverage diff viewer
-        // and unhiding the csets
-        newCset.hidden = false;
-        newCset.coverage = {
-          ...coverageData,
-          hgRev: `${HG_HOST}/mozilla-central/rev/${cset.node}`,
-          ccovBackend: `${CCOV_BACKEND}/coverage/changeset/${cset.node}`,
-          pushlog: `${HG_HOST}/pushloghtml?changeset=${coverageData.build_changeset}`,
-          codecov: `https://codecov.io/gh/marco-c/gecko-dev/commit/${coverageData.git_build_changeset}`,
-          gh: `https://github.com/mozilla/gecko-dev/commit/${coverageData.git_build_changeset}`,
-        };
-        const result = coverageSummaryText(coverageData);
-        newCset.summary = result.text;
-        newCset.summaryClassName = result.className;
-      } else {
-        console.error(`No overall_cur: ${coverageData}`);
-      }
+      coverage = transformCoverageData(await res.json());
     } else if (res.status === 500) {
-      newCset.summary = res.statusText;
+      coverage = { summary: res.statusText };
     } else {
-      console.log(`Unexpected HTTP code (${res.status}) for ${newCset}`);
+      console.log(`Unexpected HTTP code (${res.status}) for ${coverage}`);
     }
-    return newCset;
+    coverage.node = node;
   } catch (e) {
     console.log(e);
-    console.log(`Failed to fetch data for ${cset.node}`);
-    return cset;
+    console.log(`Failed to fetch data for ${node}`);
   }
+  return coverage;
+};
+
+export const getCoverage = async (changesets) => {
+  // XXX: Add local caching in this function
+  const coverage = Promise.all(changesets.map(async cset =>
+    getChangesetCoverage(cset.node)));
+  return coverage;
 };
 
 export const fileRevisionWithActiveData = async (revision, path, repoPath) => {

src/components/summary.jsx
@@ -1,6 +1,6 @@
 import ChangesetInfo from './changesetInfo';
 
-const ChangesetsViewer = ({ changesets }) => (
+const Summary = ({ changesets, coverage }) => (
   <table className="changeset-viewer">
     <tbody>
       <tr>
@@ -13,10 +13,12 @@ const ChangesetsViewer = ({ changesets }) => (
         <ChangesetInfo
           key={node}
           changeset={changesets[node]}
+          summary={coverage[node].summary}
+          summaryClassName={coverage[node].summaryClassName}
         />
       ))}
     </tbody>
   </table>
 );
 
-export default ChangesetsViewer;
+export default Summary;

src/components/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import ChangesetsViewerContainer from '../containers/summaryViewer';
+import AppDisclaimer from './disclaimer';
 import DiffViewerContainer from '../containers/diffViewer';
 import FileViewerContainer from '../containers/fileViewer';
+import GitHubRibbon from './githubRibbon';
+import SummaryContainer from '../containers/summary';
 import settings from '../settings';
 import { clearCache } from '../utils/localCache';
 import '../style.css';
-import AppDisclaimer from './disclaimer';
-import GitHubRibbon from './githubRibbon';
 
 // Main component
 export default () => (
@@ -20,7 +20,7 @@ export default () => (
         <div className="changesets-viewer">
           <GitHubRibbon />
           <AppDisclaimer />
-          <ChangesetsViewerContainer
+          <SummaryContainer
             repoName={settings.FIREFOX_REPO}
           />
         </div>
@@ -30,7 +30,7 @@ export default () => (
       path="/changeset/:id"
       render={({ match }) => (
         <DiffViewerContainer
-          changeset={match.params.id}
+          node={match.params.id}
           repoName={settings.FIREFOX_REPO}
         />
       )}

src/components/changesetInfo.jsx
@@ -1,21 +1,24 @@
 import ChangesetDescription from './changesetDescription';
+import { hgDiffUrl } from '../utils/hg';
+
 import eIcon from '../static/noun_205162_cc.png';
 import dummyIcon from '../static/dummyIcon16x16.png';
 
-const ChangesetInfo = ({ changeset }) => {
+const handleClick = (e, node) => {
+  if (e.target.tagName.toLowerCase() === 'td') {
+    window.open(`/#/changeset/${node}`, '_blank');
+  } else {
+    e.stopPropagation();
+  }
+};
+
+const ChangesetInfo = ({ changeset, summary, summaryClassName }) => {
   const {
-    authorInfo, desc, hidden, bzUrl, node, summary, summaryClassName,
+    authorInfo, desc, bzUrl, node,
   } = changeset;
-  const hgUrl = changeset.coverage.hgRev;
-  const handleClick = (e) => {
-    if (e.target.tagName.toLowerCase() === 'td') {
-      window.open(`/#/changeset/${node}`, '_blank');
-    } else {
-      e.stopPropagation();
-    }
-  };
+
   return (
-    <tr className={(hidden) ? 'hidden-changeset' : 'changeset'} onClick={e => handleClick(e)}>
+    <tr className="changeset" onClick={e => handleClick(e, node)}>
       <td className="changeset-author">
         {(authorInfo.email) ?
           <a href={`mailto: ${authorInfo.email}`}>
@@ -25,9 +28,7 @@ const ChangesetInfo = ({ changeset }) => {
         <span className="changeset-eIcon-align">{authorInfo.name.substring(0, 60)}</span>
       </td>
       <td className="changeset-hg">
-        {(hgUrl) ?
-          <a href={hgUrl} target="_blank">{node.substring(0, 12)}</a>
-          : <span>{node.substring(0, 12)}</span>}
+        <a href={hgDiffUrl(node)} target="_blank">{node.substring(0, 12)}</a>
       </td>
       <td>
         <ChangesetDescription description={desc} bzUrl={bzUrl} />

src/components/diffViewer.jsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom';
 import hash from '../utils/hash';
 import settings from '../settings';
+import { githubUrl, codecovUrl, ccovBackendUrl } from '../utils/coverage';
+import { hgDiffUrl, pushlogUrl } from '../utils/hg';
 
 const DiffViewer = ({
-  appError, coverage, parsedDiff, summary,
+  appError, coverage, parsedDiff, repoName,
 }) => (
   <div className="codecoverage-diffviewer">
     <div className="return-home">
       <Link to="/" href="/">Return to main page</Link>
     </div>
     <span className="error_message">{appError}</span>
-    {(coverage && summary !== settings.STRINGS.PENDING) && (
+    {(coverage && coverage.summary !== settings.STRINGS.PENDING) && (
       <div>
         <CoverageMeta
-          coverage={coverage}
-          summary={summary}
-        />,
+          node={coverage.node}
+          overallCoverage={coverage.overall_cur}
+          repoName={repoName}
+          summary={coverage.summary}
+        />
         {parsedDiff.map((diffBlock) => {
           // We only push down the subset of code coverage data
           // applicable to a file
@@ -27,39 +31,42 @@ const DiffViewer = ({
             key={path}
             path={path}
           />);
-        })},
+        })}
         <CoverageFooter
-          coverage={coverage}
+          gitBuildCommit={coverage.git_build_changeset}
+          hgNode={coverage.node}
         />
       </div>
     )}
   </div>
 );
 
-const CoverageMeta = ({ coverage, summary }) => (
+const CoverageMeta = ({
+  node, overallCoverage, repoName, summary,
+}) => (
   <div className="coverage-meta">
     <div className="coverage-meta-row">
       <span className="meta">
-        {`Current coverage: ${coverage.overall_cur.substring(0, 4)}%`}
+        {`Current coverage: ${overallCoverage.substring(0, 4)}%`}
       </span>
       <span className="meta meta-right">
-        <a href={coverage.pushlog} target="_blank">Push Log</a>
+        <a href={pushlogUrl(repoName, node)} target="_blank">Push Log</a>
       </span>
     </div>
     <div className="coverage-meta-row">
       <span className="meta">{summary}</span>
       <span className="meta meta-right">
-        <a href={coverage.hgRev} target="_blank">Hg Diff</a>
+        <a href={hgDiffUrl(repoName, node)} target="_blank">Hg Diff</a>
       </span>
     </div>
   </div>
 );
 
-const CoverageFooter = ({ coverage }) => (
+const CoverageFooter = ({ gitBuildCommit, hgNode }) => (
   <div className="meta-footer">
-    <a href={coverage.gh} target="_blank">GitHub</a>
-    <a href={coverage.codecov} target="_blank">Codecov</a>
-    <a href={coverage.ccovBackend} target="_blank">Coverage Backend</a>
+    <a href={githubUrl(gitBuildCommit)} target="_blank">GitHub</a>
+    <a href={codecovUrl(gitBuildCommit)} target="_blank">Codecov</a>
+    <a href={ccovBackendUrl(hgNode)} target="_blank">Coverage Backend</a>
   </div>
 );
 
@@ -71,7 +78,7 @@ const DiffFile = ({
     <div className="file-summary">
       <div className="file-path">
         <Link
-          class="diff-viewer-link"
+          className="diff-viewer-link"
           to={`/file?revision=${buildRev}&path=${path}`}
           href={`/file?revision=${buildRev}&path=${path}`}
         >

src/containers/diffViewer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { orderBy } from 'lodash';
 
 import DiffViewer from '../components/diffViewer';
-import { csetWithCcovData } from '../utils/coverage';
+import { getChangesetCoverage } from '../utils/coverage';
 import { getDiff } from '../utils/hg';
 import settings from '../settings';
 
@@ -30,27 +30,25 @@ export default class DiffViewerContainer extends Component {
     super(props);
     this.state = {
       appError: undefined,
-      csetMeta: {
-        coverage: undefined,
-      },
+      coverage: undefined,
       parsedDiff: [],
     };
   }
 
   componentDidMount() {
-    const { changeset } = this.props;
-    Promise.all([this.fetchSetCoverageData(changeset), this.fetchSetDiff(changeset)]);
+    const { node } = this.props;
+    Promise.all([this.fetchSetCoverageData(node), this.fetchSetDiff(node)]);
   }
 
-  async fetchSetCoverageData(changeset) {
+  async fetchSetCoverageData(node) {
     try {
-      const csetMeta = await csetWithCcovData({ node: changeset });
-      if (csetMeta.summary === settings.STRINGS.PENDING) {
+      const coverage = await getChangesetCoverage(node);
+      if (coverage.summary === settings.STRINGS.PENDING) {
         this.setState({
           appError: 'The coverage data is still pending. Try again later.',
         });
       }
-      this.setState({ csetMeta });
+      this.setState({ coverage });
     } catch (error) {
       console.error(error);
       this.setState({
@@ -59,9 +57,9 @@ export default class DiffViewerContainer extends Component {
     }
   }
 
-  async fetchSetDiff(changeset) {
+  async fetchSetDiff(node) {
     try {
-      const text = await (await getDiff(changeset)).text();
+      const text = await (await getDiff(node)).text();
       this.setState({ parsedDiff: parse(text) });
     } catch (e) {
       if ((e instanceof TypeError) && (e.message === 'Failed to fetch')) {
@@ -80,14 +78,15 @@ export default class DiffViewerContainer extends Component {
   }
 
   render() {
-    const { appError, csetMeta, parsedDiff } = this.state;
-    const sortedDiff = sortByPercent(parsedDiff, csetMeta.coverage);
+    const { appError, coverage, parsedDiff } = this.state;
+    const sortedDiff = sortByPercent(parsedDiff, coverage);
 
     return (
       <DiffViewer
-        {...csetMeta}
         appError={appError}
+        coverage={coverage}
         parsedDiff={sortedDiff}
+        repoName={this.props.repoName}
       />
     );
   }

src/settings.js
@@ -1,11 +1,13 @@
 const settings = {
   ACTIVE_DATA: 'https://activedata.allizom.org',
-  BZ_URL: 'http://bugzilla.mozilla.org',
+  BACKEND: 'https://uplift.shipit.staging.mozilla-releng.net',
+  BZ_URL: 'https://bugzilla.mozilla.org',
   CACHE_CONFIG: {
     SECONDS_TO_EXPIRE: 1 * 60 * 60, // 1 hour
     ENABLED: process.env.ENABLE_CACHE === 'true' || process.env.NODE_ENV === 'production',
   },
   CCOV_BACKEND: 'https://uplift.shipit.staging.mozilla-releng.net',
+  CODECOV_GECKO_DEV: 'https://codecov.io/gh/marco-c/gecko-dev',
   COVERAGE_THRESHOLDS: {
     low: {
       threshold: 20,
@@ -20,6 +22,7 @@ const settings = {
     },
   },
   FIREFOX_REPO: 'mozilla-central',
+  GH_GECKO_DEV: 'https://github.com/mozilla/gecko-dev',
   GITHUB_RIBBON: 'https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png',
   HG_HOST: 'https://hg.mozilla.org',
   MIN_REVISION_LENGTH: 5,
@@ -29,7 +32,7 @@ const settings = {
     LOADING: 'Loading...',
     PENDING: 'Pending',
   },
-  HG_DAYS_AGO: new Date((new Date()).getTime() - (2 * 24 * 60 * 60 * 1000))
+  HG_DAYS_AGO: new Date((new Date()).getTime() - (1 * 24 * 60 * 60 * 1000))
     .toISOString().substring(0, 10),
 };
 

commit 66450ac637faf4c9930a8667f13d0485764f9c90

Issue #159 - Fix: Regression from 18cc0ed2dcd08a894acb7b6974e2fca6f085918b
In the past we used to determine if a changeset was to show by doing this check:

```javascript
mapToArray(changesets).filter(c => c.hidden === false)
```

We then switched to:

```javascript
coverageMap[cset.node].summary !== PENDING
```

This adds a `show` property and we check on it:

```javascript
coverageMap[cset.node].show
```

src/containers/summary.jsx
@@ -109,7 +109,7 @@ export default class SummaryContainer extends Component {
 
     const viewableCsetsMap = {};
     changesets.forEach((cset) => {
-      if (coverageMap[cset.node].summary !== PENDING) {
+      if (coverageMap[cset.node].show) {
         viewableCsetsMap[cset.node] = cset;
       }
     });

src/utils/coverage.js
@@ -144,16 +144,17 @@ export const getChangesetCoverage = async (node) => {
   if (!node) {
     throw Error(`No node for cset: ${node}`);
   }
-  let coverage;
+  let coverage = { show: false };
   try {
     const res = await queryChangesetCoverage(node);
     if (res.status === 202) {
       // This is the only case when we poll again
-      coverage = { summary: settings.STRINGS.PENDING };
+      coverage.summary = settings.STRINGS.PENDING;
     } else if (res.status === 200) {
       coverage = transformCoverageData(await res.json());
+      coverage.show = true;
     } else if (res.status === 500) {
-      coverage = { summary: res.statusText };
+      coverage.summary = res.statusText;
     } else {
       console.log(`Unexpected HTTP code (${res.status}) for ${coverage}`);
     }