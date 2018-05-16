de2b1384cce868a9047ce49a83cfb64831999f50
Armen Zambrano G <armenzg@gmail.com>
2018-05-16 11:21:38 -0400

    Issue #57 - Allow sorting changesets per coverage score
    Add mdi-react dependency.

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,24 +1,79 @@
+import { Component } from 'react';
+import MenuDownIcon from 'mdi-react/MenuDownIcon';
+import MenuRightIcon from 'mdi-react/MenuRightIcon';
+import MenuUpIcon from 'mdi-react/MenuUpIcon';
 import ChangesetInfo from './changesetInfo';
+import {
+  sortChangesetsByCoverage,
+  sortChangesetsNewestFirst,
+  sortingMethods,
+} from '../utils/data';
 
-const Summary = ({ changesetsCoverage, sortedChangesets }) => (
-  <table className="changeset-viewer">
-    <tbody>
-      <tr>
-        <th>Author</th>
-        <th>Changeset</th>
-        <th>Description</th>
-        <th>Coverage summary</th>
-      </tr>
-      {sortedChangesets.map(changeset => (
-        <ChangesetInfo
-          key={changeset.node}
-          changeset={changeset}
-          summary={changesetsCoverage[changeset.node].summary}
-          summaryClassName={changesetsCoverage[changeset.node].summaryClassName}
-        />
-      ))}
-    </tbody>
-  </table>
-);
+export default class Summary extends Component {
+  state = {
+    sortingMethod: sortingMethods.DATE,
+    sortingReversed: false,
+  }
 
-export default Summary;
+  onClickNextSorting() {
+    if (this.state.sortingMethod === sortingMethods.DATE) {
+      this.setState({ sortingMethod: sortingMethods.COVERAGE });
+    } else if (!this.state.sortingReversed) {
+      this.setState({ sortingReversed: true });
+    } else {
+      this.setState({
+        sortingMethod: sortingMethods.DATE,
+        sortingReversed: false,
+      });
+    }
+  }
+
+  displaySortingStatusIcon() {
+    const { sortingMethod, sortingReversed } = this.state;
+    if (sortingMethod === sortingMethods.DATE) {
+      return <MenuRightIcon />;
+    }
+    if (sortingMethod === sortingMethods.COVERAGE) {
+      return sortingReversed ? <MenuDownIcon /> : <MenuUpIcon />;
+    }
+    return undefined;
+  }
+
+  sortedChangesets() {
+    const { changesets, changesetsCoverage } = this.props;
+    if (this.state.sortingMethod === sortingMethods.DATE) {
+      return sortChangesetsNewestFirst(changesets, changesetsCoverage);
+    }
+    return sortChangesetsByCoverage(changesets, changesetsCoverage, this.state.sortingReversed);
+  }
+
+  render() {
+    const { changesets, changesetsCoverage } = this.props;
+
+    const sortedChangesets = this.sortedChangesets();
+
+    return (
+      <table className="changeset-viewer">
+        <tbody>
+          <tr>
+            <th>Author</th>
+            <th>Changeset</th>
+            <th>Description</th>
+            <th onClick={() => this.onClickNextSorting()}>
+              {this.displaySortingStatusIcon()}
+              <span>Coverage summary</span>
+            </th>
+          </tr>
+          {sortedChangesets.map(({ node }) => (
+            <ChangesetInfo
+              key={node}
+              changeset={changesets[node]}
+              summary={changesetsCoverage[node].summary}
+              summaryClassName={changesetsCoverage[node].summaryClassName}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
--- src/containers/summary.jsx
+++ src/containers/summary.jsx
@@ -4,12 +4,8 @@ import ReactInterval from 'react-interval';
 import Summary from '../components/summary';
 import ChangesetFilter from '../components/changesetFilter';
 import settings from '../settings';
-import {
-  loadCoverageData,
-  pollPendingChangesets,
-  sortChangesets,
-  sortingMethods,
-} from '../utils/data';
+import { pollPendingChangesets } from '../utils/coverage';
+import { loadCoverageData } from '../utils/data';
 
 const { LOADING } = settings.STRINGS;
 
@@ -31,7 +27,6 @@ export default class SummaryContainer extends Component {
       descriptionFilterValue: '',
       pollingEnabled: false, // We don't start polling until we're ready
       timeout: 10000, // How often we poll for csets w/o coverage status
-      sortingMethod: sortingMethods.DATE,
     };
     this.onFilterByDescription = this.onFilterByDescription.bind(this);
   }
@@ -47,6 +42,7 @@ export default class SummaryContainer extends Component {
 
   async initializeData() {
     try {
+      // This will either fetch the data or grab it from the cache
       const { changesets, changesetsCoverage, summary } = await loadCoverageData();
       this.setState({
         changesets,
@@ -65,11 +61,10 @@ export default class SummaryContainer extends Component {
   }
 
   // We poll on an interval for coverage for csets without it
-  async pollPending(changesetsCoverage) {
-    console.debug('We are going to poll again for coverage data.');
+  async pollPending(coverage) {
     try {
-      const { csetsCoverage, polling } = await pollPendingChangesets(changesetsCoverage);
-      this.setState({ changesetsCoverage: csetsCoverage, pollingEnabled: polling });
+      const { changesetsCoverage, pollingEnabled } = await pollPendingChangesets(coverage);
+      this.setState({ changesetsCoverage, pollingEnabled });
     } catch (e) {
       this.setState({ pollingEnabled: false });
     }
@@ -89,8 +84,9 @@ export default class SummaryContainer extends Component {
       return (<div className="error-message">{errorMessage}</div>);
     }
 
-    const sortedChangesets =
-      sortChangesets(changesets, changesetsCoverage, this.state.sortingMethod);
+    const ready =
+      Object.keys(changesets).length > 0 &&
+      Object.keys(changesetsCoverage).length > 0;
 
     return (
       <div>
@@ -110,22 +106,19 @@ export default class SummaryContainer extends Component {
           value={descriptionFilterValue}
           onChange={this.onFilterByDescription}
         />
-        {sortedChangesets.length > 0 && (
+        {ready && (
           <Summary
+            changesets={changesets}
             changesetsCoverage={changesetsCoverage}
-            sortedChangesets={sortedChangesets}
           />
         )}
-        {(!pollingEnabled &&
-          (Object.keys(changesetsCoverage).length > 0)
-          && sortedChangesets.length === 0) && (
-            <p style={{ textAlign: 'center', fontWeight: 'bold' }}>
-              <span>There is currently no coverage data to show. Please </span>
-              <a href={`${settings.REPO}/issues/new`} target="_blank">file an issue</a>.
-            </p>
-          )
-        }
-        {pollingEnabled &&
+        {!pollingEnabled && ready && (
+          <p style={{ textAlign: 'center', fontWeight: 'bold' }}>
+            <span>There is currently no coverage data to show. Please </span>
+            <a href={`${settings.REPO}/issues/new`} target="_blank">file an issue</a>.
+          </p>
+        )}
+        {!pollingEnabled &&
           (<h3 className="loading">{LOADING}</h3>)
         }
       </div>
