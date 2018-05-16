cc74cecd8135536114ad4080b9c974b1b2c4f9fe
Armen Zambrano G <armenzg@gmail.com>
2018-05-16 15:17:33 -0400

    Issue 45 - Do not show unsupported files in the Diff view

--- src/utils/data.js
+++ src/utils/data.js
@@ -9,3 +9,17 @@ export const arrayToMap = (csets = []) => {
 export const mapToArray = (csets = {}) => (
   Object.keys(csets).map(node => csets[node])
 );
+
+export const filterUnsupportedExtensions = (parsedDiff = {}, supportedExtensions = []) => {
+  if (supportedExtensions.length === 0) {
+    return parsedDiff;
+  }
+  const newDiff = [];
+  parsedDiff.forEach((p) => {
+    const extensionTo = p.to.split('.').pop();
+    if (supportedExtensions.indexOf(extensionTo) >= 0) {
+      newDiff.push(p);
+    }
+  });
+  return newDiff;
+};
