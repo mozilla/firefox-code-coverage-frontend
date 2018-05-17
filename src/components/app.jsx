a2731ae58d6c01ff6c42232d43215c36de520239
Armen Zambrano G <armenzg@gmail.com>
2018-05-17 16:56:40 -0400

    refactor: Move routing logic into Routes component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,46 +1,9 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 
-import AppDisclaimer from './disclaimer';
-import DiffViewerContainer from '../containers/diffViewer';
-import FileViewerContainer from '../containers/fileViewer';
-import GitHubRibbon from './githubRibbon';
-import SummaryContainer from '../containers/summary';
-import { clearCache } from '../utils/localCache';
+import Routes from './routes';
 import '../style.css';
 
 // Main component
 export default () => (
-  <div className="app">
-    <Route
-      exact
-      path="/"
-      render={() => (
-        <div className="changesets-viewer">
-          <GitHubRibbon />
-          <AppDisclaimer />
-          <SummaryContainer />
-        </div>
-      )}
-    />
-    <Route
-      path="/changeset/:id"
-      render={({ match }) => (
-        <DiffViewerContainer node={match.params.id} />
-      )}
-    />
-    <Route
-      path="/file"
-      component={FileViewerContainer}
-    />
-    <Route
-      path="/clear-cache"
-      render={() => {
-        if (clearCache()) {
-          return (<p>The local database has been cleared.</p>);
-        }
-        return (<p>Failed to clear the local DB.</p>);
-      }}
-    />
-  </div>
+  <Routes />
 );
