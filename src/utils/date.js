6cb83eba90153a5b103278d8bd2e7a938c76f843
Armen Zambrano G <armenzg@gmail.com>
2018-05-18 10:01:48 -0400

    Add daysAgoDate helper function

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,8 @@
-const dateFromUnixTimestamp = (unixTimestamp) => {
+export const dateFromUnixTimestamp = (unixTimestamp) => {
   const date = new Date(unixTimestamp * 1000);
   return `${date.toLocaleString()}`;
 };
 
-export default dateFromUnixTimestamp;
+export const daysAgoDate = numberOfDays =>
+  new Date((new Date()).getTime() - (numberOfDays * 24 * 60 * 60 * 1000))
+    .toISOString().substring(0, 10);
