4a676c668fa7146105cf4ebc06c2e6d853c57531
Armen Zambrano G <armenzg@gmail.com>
2018-05-22 09:47:28 -0400

    Issue #69 - Allow folding & unfolding of files in the diff view

--- src/components/diffFile.jsx
+++ src/components/diffFile.jsx
@@ -1,31 +1,65 @@
+import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MenuRightIcon from 'mdi-react/MenuRightIcon';
+import MenuDownIcon from 'mdi-react/MenuDownIcon';
 import DiffBlock from './diffBlock';
 
 /* A DiffLine contains all diff changes for a specific file */
-const DiffFile = ({
-  buildRev, diffBlock, fileCoverageDiffs, path,
-}) => (
-  <div className="diff-file">
-    <div className="file-summary">
-      <div className="file-path">
-        <Link
-          className="diff-viewer-link"
-          to={`/file?revision=${buildRev}&path=${path}`}
-          href={`/file?revision=${buildRev}&path=${path}`}
+class DiffFile extends Component {
+  state = {
+    hiddenBlocks: false,
+  }
+
+  componentDidMount() {
+    // Collapse by default files which don't have any coverage information
+    if (!this.state.hiddenBlocks && !this.props.fileCoverageDiffs) {
+      // eslint-disable-next-line react/no-did-mount-set-state
+      this.setState({ hiddenBlocks: true });
+    }
+  }
+
+  onClickHideOrShow() {
+    this.setState({ hiddenBlocks: !this.state.hiddenBlocks });
+  }
+
+  render() {
+    const { hiddenBlocks } = this.state;
+    const { buildRev, diffBlock, fileCoverageDiffs, path } = this.props;
+
+    return (
+      <div className="diff-file">
+        <div
+          className="file-summary"
+          onClick={() => this.onClickHideOrShow()}
+          role="button"
+          tabIndex={0}
         >
-          {path}
-        </Link>
+          <div className="file-path">
+            {hiddenBlocks ? (
+              <MenuRightIcon />
+            ) : (
+              <MenuDownIcon />
+            )}
+            <Link
+              className="diff-viewer-link"
+              to={`/file?revision=${buildRev}&path=${path}`}
+              href={`/file?revision=${buildRev}&path=${path}`}
+            >
+              {path}
+            </Link>
+          </div>
+        </div>
+        {!hiddenBlocks && diffBlock.chunks.map(block => (
+          <DiffBlock
+            block={block}
+            filePath={path}
+            fileDiffs={fileCoverageDiffs}
+            key={block.content}
+          />
+        ))}
       </div>
-    </div>
-    {diffBlock.chunks.map(block => (
-      <DiffBlock
-        block={block}
-        filePath={path}
-        fileDiffs={fileCoverageDiffs}
-        key={block.content}
-      />
-    ))}
-  </div>
-);
+    );
+  }
+}
 
 export default DiffFile;
