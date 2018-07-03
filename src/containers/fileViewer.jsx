6870dbb2a159f33cfc6b6ed9603eeb772ebe0d82
Jan Keromnes <janx@linux.com>
2018-07-03 16:36:14 +0200

    File viewer: Support 'hashchange' navigation events

--- src/containers/fileViewer.jsx
+++ src/containers/fileViewer.jsx
@@ -18,8 +18,20 @@ export default class FileViewerContainer extends Component {
   }
 
   componentDidMount() {
-    const { revision, path } = this.state;
-    this.fetchData(revision, path);
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.search === prevProps.location.search) {
+      return;
+    }
+    // Reset the state and fetch new data
+    const newState = this.parseQueryParams();
+    newState.coverage = undefined;
+    newState.parsedFile = undefined;
+    // eslint-disable-next-line react/no-did-update-set-state
+    this.setState(newState);
+    this.fetchData();
   }
 
   setSelectedLine(selectedLineNumber) {
@@ -31,7 +43,8 @@ export default class FileViewerContainer extends Component {
     }
   }
 
-  fetchData(revision, path, repoPath = 'mozilla-central') {
+  fetchData(repoPath = 'mozilla-central') {
+    const { revision, path } = this.state;
     // Get source code from hg
     const fileSource = async () => {
       this.setState({ parsedFile: (await rawFile(revision, path, repoPath)) });
